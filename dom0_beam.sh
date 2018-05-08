763d76f04facdcece45bc3d228d876580af4568b
Jonny C <35306921+Cr33dC0d3r@users.noreply.github.com>
2018-05-08 17:37:34 +0200

    Create dom0_beam.sh

---
+++ dom0_beam.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Dom0 Beam
+
+src_dir=$1		#Case Sensitive!
+dest_dir=$3 	#Case Sensitive!
+src_vm=$2
+dest_vm=$4
+
+# Now we need to execute qubes command to pass I/O
+# using a pipe from one VM to another
+
+echo "File Transfer established: $src_vm -> $dest_vm !"
+
+qvm-run -p $src_dir "cat $src_dir" | qvm-run -p $dest_vm "cat > $dest_dir"
+
+echo "File Transmitted!"
