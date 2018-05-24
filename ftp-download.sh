397d964ebc7f958f9ac5b3d131e54cb086300404
Jonny C <35306921+Cr33dC0d3r@users.noreply.github.com>
2018-05-24 11:07:08 +0200

    Update ftp-download.sh

--- ftp-download.sh
+++ ftp-download.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
+#This script downloads two specific files from an static dir on a ftp-server
 HOST='x.x.x.x'
 USER='AppVM1'
 PASSWD='********'
 
-FILE=$1
+FILE=$1                 #Enter the name of the encrypted file
+SIG=$2                  #Enter the name of the detached signature
 ftp -n -v $HOST <<EOT
 ascii
 user $USER $ PASSWD
